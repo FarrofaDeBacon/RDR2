a1426fcdc67d4570356cc885aeb041501fe58b02
Steel Server <steel-server@example.com>
2026-07-15 09:38:30 -0300

    fix(mapmenu): declarar nomes exatos com hash no manifest para evitar erros de globbing

--- projetos/resources/[framework]/fdb-mapmenu/fxmanifest.lua
+++ projetos/resources/[framework]/fdb-mapmenu/fxmanifest.lua
@@ -20,8 +20,8 @@ server_scripts {
 
 files {
     'ui/public/index.html',
-    'ui/public/assets/*.js',
-    'ui/public/assets/*.css',
+    'ui/public/assets/index-Dgfg-xd0.js',
+    'ui/public/assets/index-CzUDblFE.css',
     -- Declaração explícita por nível de zoom para garantir empacotamento completo de subpastas profundas no RedM
     'tiles/0/**/*.webp',
     'tiles/1/**/*.webp',
